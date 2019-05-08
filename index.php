6bd481c8b4f0f7b403d4498ddef35d6b3f3cf51c
VANG RYAN <150075167@cart.ads>
2019-05-08 09:23:05 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Champloo</title>
+    <link rel="stylesheet" href="champloo.css">
+</head>
+<body>
+    <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a href="">About</a>
+        <a href="">Stickers</a>
+        <a href="">Contact</a>
+    </div>
+    
+    <div class="hero-image">
+  <div class="hero-text">
+    <h1>Champloo</h1>
+    <button>Shop now</button>
+  </div>
+</div>
+
+</body>
+</html>
