f8e8ca4e7150fbf25128d2189e19eabceb370b3e
VANG RYAN <150075167@cart.ads>
2019-05-14 10:24:48 -0700

    finished home page/added sticker page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Champloo</title>
+    <title>Champloo-Home</title>
     <link rel="stylesheet" href="champloo.css">
 </head>
 <body>
@@ -14,7 +14,9 @@
             
             <li><a href="">About</a></li>
             
-            <li><a href="">Stickers</a></li>
+            <li><img width="35px" height="110px" src="images/Champloo%20Icon%20New.png" alt="Champloo logo"></li>
+            
+            <li><a href="stickers.php">Stickers</a></li>
             
             <li><a href="">Contact</a></li>
             
@@ -25,14 +27,14 @@
     <div class="hero-image">
   <div class="hero-text">
     <h1>Champloo</h1>
-    <a href="">Shop Now</a>
+    <a href="stickers.php">Shop Now</a>
   </div>
 </div>
 <div class="flex-container">
     <div class="row">
     <h2>Champloo Stickers</h2>
     <p>Only the best custom-made stickers here.</p>
-    <a href="">Get Stickers</a>
+    <a href="stickers.php">Get Stickers</a>
     </div>
     <div>
         <img width="400px" height="400px" src="images/BEANIE%20BOY%20Color.webp" alt="beanie boy">
@@ -46,11 +48,18 @@
 </div>
 
 <footer>
+ <div class="footer-flex">
+  <div>
    <a href="index.php">Home</a>
     <a href="">About</a>
-    <a href="">Stickers</a>
+    <a href="stickers.php">Stickers</a>
     <a href="">Contact</a>
-
+  </div>
+  
+  <div>
+      <h2>Champloo</h2>
+  </div>
+    </div>
 </footer>
 </body>
 </html>
---
+++ stickers.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Champloo-Home</title>
+    <link rel="stylesheet" href="champloo.css">
+</head>
+<body>
+    <nav>
+       
+        <ul>
+            
+            <li><a href=index.php>Home</a></li>
+            
+            <li><a href="">About</a></li>
+            
+            <li><img width="35px" height="110px" src="images/Champloo%20Icon%20New.png" alt="Champloo logo"></li>
+            
+            <li><a class="active" href="stickers.php">Stickers</a></li>
+            
+            <li><a href="">Contact</a></li>
+            
+        </ul>
+     
+    </nav>
+    
+    <div class="flex-stickers">
+        <div>
+            <img width="300px" height="300px" src="images/BEANIE%20BOY%20Color.webp" alt="Beanie Boy">
+            <p id="p1">Beanie Boy</p>
+            <p>$3.00</p>
+        </div>
+        <div>
+            <img width="300px" height="300px" src="images/Empty.webp" alt="Empty">
+            <p id="p1">Empty</p>
+            <p>$3.00</p>
+        </div>
+        <div>
+            <img width="300px" height="300px" src="images/Nice!.webp" alt="Nice!">
+            <p id="p1">Nice!</p>
+            <p>$3.00</p>
+        </div>
+        <div>
+            <img width="300px" height="300px" src="images/racoon.webp" alt="Raccoon">
+            <p id="p1">Raccoon</p>
+            <p>$3.00</p>
+        </div>
+        <div>
+            <img width="300px" height="300px" src="images/Work!.webp" alt="Work!">
+            <p id="p1">Work!</p>
+            <p>$3.00</p>
+        </div>
+        <div>
+            <img width="300px" height="300px" src="images/yellow%20hat.webp" alt="Yellow Hat">
+            <p id="p1">Yellow Hat</p>
+            <p>$3.00</p>
+        </div>
+        <div>
+            <img width="300px" height="300px" src="images/Champloo%20Cup%20Noodle%20new.png" alt="Cup Noodles">
+            <p id="p1">cup noodles</p>
+            <p>$3.00</p>
+        </div>
+    </div>
+    
+<footer>
+ <div class="footer-flex">
+  <div>
+   <a href="index.php">Home</a>
+    <a href="">About</a>
+    <a href="stickers.php">Stickers</a>
+    <a href="">Contact</a>
+  </div>
+  
+  <div>
+      <h2>Champloo</h2>
+  </div>
+    </div>
+</footer>
+</body>
+</html>
